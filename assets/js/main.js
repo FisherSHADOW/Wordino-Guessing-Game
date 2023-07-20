8be8686a8f16f45504b5c2fbcb41796a280acfac
Fisher__SHADOW <hadi.h9589@gmail.com>
2023-07-20 04:53:39 +0330

    files added

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// main page text animation
+function setTextAnimation(delay, duration, strokeWidth, timingFunction, strokeColor,repeat) {
+    let paths = document.querySelectorAll("path");
+    let mode=repeat?'infinite':'forwards'
+    for (let i = 0; i < paths.length; i++) {
+        const path = paths[i];
+        const length = path.getTotalLength();
+        path.style["stroke-dashoffset"] = `${length}px`;
+        path.style["stroke-dasharray"] = `${length}px`;
+        path.style["stroke-width"] = `${strokeWidth}px`;
+        path.style["stroke"] = `${strokeColor}`;
+        path.style["animation"] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+        path.style["animation-delay"] = `${i * delay}s`;
+    }
+}
+setTextAnimation(0.1,7,2,'ease-in','#ffc857',false);
+
+//end of main page text animation 
